925432ddfb7fe995a25291ad8ebdf2e29108f73d
Kate Storey-Fisher <k.sf@nyu.edu>
2021-03-09 00:57:12 -0500

    add extra credit solution

--- hw2/fast-sin.cpp
+++ hw2/fast-sin.cpp
@@ -58,10 +58,15 @@ void sin4_taylor(double* sinx, const double* x) {
 
 
 void sin4_taylor_extended(double* sinx, const double* x) {
-  // If outside interval, compute cosine taylor series
+  // To compute sinx at any x value, we check the x location
+  // and choose whether to compute the sin or cos, based on
+  // Euler's formula.
+  // [This takes a long time! There must be a better way 
+  // but I can't think of it right now.]
   for (int i = 0; i < 4; i++) {
-
+    // n is how many pi/2 shifts we need to get to the small x regime
     int n = floor((x[i]+M_PI/4)/(M_PI/2));
+    // xm is x shifted to the small x regime
     double xm = x[i] - n*M_PI/2;
     // If n is odd (x in [pi/4,3pi/4], [5pi/4, 7pi/4]...) use cosine
     if (abs(n)%2==1) { 
@@ -106,6 +111,83 @@ void sin4_taylor_extended(double* sinx, const double* x) {
     }
   }
 }
+
+void sin4_intrin_extended(double* sinx, const double* x) {
+  // Here I implement a method for sinx that can take any x,
+  // not just in the range -pi/4, pi/4. This relies on a trick
+  // using Euler's formula where we shift the angle and compute
+  // cosx instead. Because of the vectorization, we here compute
+  // both sinx and cosx for all for values of x, and at the end
+  // figure out which we need.
+  // [This is super inefficient and takes a long time! But I 
+  // can't think of a better way right now.]
+#if defined(__AVX__)
+  double sinres[4];
+  double cosres[4];
+  double xm[4];
+  int n[4];
+  // Shift angle towards small x, keep track of how much shifting
+  // we did (n)
+  for (int i = 0; i < 4; i++) {
+    n[i] = floor((x[i]+M_PI/4)/(M_PI/2));
+    xm[i] = x[i] - n[i]*M_PI/2;
+  }
+  // sin
+  __m256d x1, x2, x3, x5, x7, x9, x11;
+  x1  = _mm256_load_pd(xm);
+  x2  = _mm256_mul_pd(x1, x1);
+  x3  = _mm256_mul_pd(x1, x2);
+  x5  = _mm256_mul_pd(x3, x2);
+  x7  = _mm256_mul_pd(x5, x2);
+  x9  = _mm256_mul_pd(x7, x2);
+  x11  = _mm256_mul_pd(x9, x2);
+
+  __m256d s = x1;
+  s = _mm256_add_pd(s, _mm256_mul_pd(x3 , _mm256_set1_pd(c3 )));
+  s = _mm256_add_pd(s, _mm256_mul_pd(x5 , _mm256_set1_pd(c5 )));
+  s = _mm256_add_pd(s, _mm256_mul_pd(x7 , _mm256_set1_pd(c7 )));
+  s = _mm256_add_pd(s, _mm256_mul_pd(x9 , _mm256_set1_pd(c9 )));
+  s = _mm256_add_pd(s, _mm256_mul_pd(x11 , _mm256_set1_pd(c11 )));
+  _mm256_store_pd(sinres, s);
+
+  // cos
+  __m256d x4, x6, x8, x10;
+  x4  = _mm256_mul_pd(x2, x2);
+  x6  = _mm256_mul_pd(x4, x2);
+  x8  = _mm256_mul_pd(x6, x2);
+  x10  = _mm256_mul_pd(x8, x2);
+
+  __m256d c = _mm256_set_pd(1.0, 1.0, 1.0, 1.0);
+  c = _mm256_add_pd(c, _mm256_mul_pd(x2 , _mm256_set1_pd(c2 )));
+  c = _mm256_add_pd(c, _mm256_mul_pd(x4 , _mm256_set1_pd(c4 )));
+  c = _mm256_add_pd(c, _mm256_mul_pd(x6 , _mm256_set1_pd(c6 )));
+  c = _mm256_add_pd(c, _mm256_mul_pd(x8 , _mm256_set1_pd(c8 )));
+  c = _mm256_add_pd(c, _mm256_mul_pd(x10 , _mm256_set1_pd(c10 )));
+  _mm256_store_pd(cosres, c);
+
+  // Depending on the number of shifts we did, store the positive
+  // or negative sin or cosine
+  for (int i = 0; i < 4; i++) {
+    int nmod = (n[i] % 4 + 4) % 4;
+    if (nmod==0){ 
+      sinx[i] = sinres[i]; 
+    }
+    else if (nmod==1){ 
+      sinx[i] = cosres[i]; 
+    }
+    else if (nmod==2){ 
+      sinx[i] = -sinres[i]; 
+    }
+    // nmod==3
+    else { 
+      sinx[i] = -cosres[i]; 
+    }
+  }
+#else
+  sin4_reference(sinx, x);
+#endif
+}
+
 
 void sin4_intrin(double* sinx, const double* x) {
   // The definition of intrinsic functions can be found at:
@@ -167,10 +249,8 @@ double err(double* x, double* y, long N) {
 
 int main() {
   Timer tt;
-  //long N = 1000000;
-  long N = 10000;
+  long N = 1000000;
   double* x = (double*) aligned_malloc(N*sizeof(double));
-  double* xext = (double*) aligned_malloc(N*sizeof(double));
   double* sinx_ref = (double*) aligned_malloc(N*sizeof(double));
   double* sinx_taylor = (double*) aligned_malloc(N*sizeof(double));
   double* sinx_intrin = (double*) aligned_malloc(N*sizeof(double));
@@ -181,15 +261,13 @@ int main() {
   for (long i = 0; i < N; i++) {
     //x[i] = (drand48()-0.5) * M_PI/2; // [-pi/4,pi/4]
     x[i] = (drand48()-0.5) * 4*M_PI; // [-pi,pi]
-    //x[i] = (drand48()-4) * M_PI/4; //[-4pi/4, -3pi/4]
     sinx_ref[i] = 0;
     sinx_taylor[i] = 0;
     sinx_intrin[i] = 0;
     sinx_vector[i] = 0;
 
-    // for extended, use interval [-pi,pi]
-    //x[i] = (drand48()-0.5) * 2*M_PI;
     sinx_taylor_extended[i] = 0;
+    sinx_intrin_extended[i] = 0;
   }
 
   // This is giving the memory address for index i of the length N
@@ -229,18 +307,25 @@ int main() {
   tt.tic();
   for (long rep = 0; rep < 1000; rep++) {
     for (long i = 0; i < N; i+=4) {
-      //sin4_taylor_extended(sinx_taylor_extended+i, xext+i);
       sin4_taylor_extended(sinx_taylor_extended+i, x+i);
     }
   }
-  printf("Taylor time:    %6.4f      Error: %e\n", tt.toc(), err(sinx_ref, sinx_taylor_extended, N));
+  printf("Taylor extended time:    %6.4f      Error: %e\n", tt.toc(), err(sinx_ref, sinx_taylor_extended, N));
+
+  tt.tic();
+  for (long rep = 0; rep < 1000; rep++) {
+    for (long i = 0; i < N; i+=4) {
+      sin4_intrin_extended(sinx_intrin_extended+i, x+i);
+    }
+  }
+  printf("Intrin extended time:    %6.4f      Error: %e\n", tt.toc(), err(sinx_ref, sinx_intrin_extended, N));
 
   aligned_free(x);
-  aligned_free(xext);
   aligned_free(sinx_ref);
   aligned_free(sinx_taylor);
   aligned_free(sinx_taylor_extended);
   aligned_free(sinx_intrin);
+  aligned_free(sinx_intrin_extended);
   aligned_free(sinx_vector);
 }
 
