96aa697c26e7e1b8522eb715183f8d45de3cfd0f
Kate Storey-Fisher <k.sf@nyu.edu>
2021-03-25 00:48:46 -0400

    some attempts at 4, no cigar yet

--- hw3/omp_solved4.c
+++ hw3/omp_solved4.c
@@ -9,20 +9,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 1048
+//#define N 4
 
 int main (int argc, char *argv[]) 
 {
 int nthreads, tid, i, j;
 double a[N][N];
+void compute_a(int i, int j, double a[N][N], int tid);
 
+printf("start\n");
+//#pragma stack, 10000000
+#pragma comment(linker, "/STACK:20000000")
 /* Fork a team of threads with explicit variable scoping */
 #pragma omp parallel shared(nthreads) private(i,j,tid,a)
   {
-
+  printf("here\n");
   /* Obtain/print thread info */
   tid = omp_get_thread_num();
+  printf("posttid\n");
   if (tid == 0) 
     {
+    printf("tid0\n");
     nthreads = omp_get_num_threads();
     printf("Number of threads = %d\n", nthreads);
     }
@@ -32,7 +39,7 @@ double a[N][N];
   for (i=0; i<N; i++)
     for (j=0; j<N; j++)
       a[i][j] = tid + i + j;
-
+  
   /* For confirmation */
   printf("Thread %d done. Last element= %f\n",tid,a[N-1][N-1]);
 
@@ -40,3 +47,9 @@ double a[N][N];
 
 }
 
+/*void compute_a(int i, int j, double a[N][N], int tid)
+{
+    for (i=0; i<N; i++)
+      for (j=0; j<N; j++)
+        a[i][j] = tid + i + j;
+}*/
