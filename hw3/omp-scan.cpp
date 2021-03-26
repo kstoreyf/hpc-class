94c5c66cda73c537173f86c5ce17877cfec5b59f
Kate Storey-Fisher <k.sf@nyu.edu>
2021-03-25 22:57:12 -0400

    omp-scan working, messy tho

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -10,20 +10,28 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
   prefix_sum[0] = 0;
   for (long i = 1; i < n; i++) {
     prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+    //printf("pcorr[%ld] = %ld\n", i, prefix_sum[i]);
   }
 }
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
   // TODO: implement multi-threaded OpenMP scan
   if (n == 0) return;
+  //for (long i=0; i<n; i++){
+  //  printf("A[%ld] = %ld\n", i, A[i]);
+  //}
   int tid, p;
   //prefix_sum[0] = 0; //shouldnt this be A[0]?? also related, in above, never get to A[n-1]
-  #pragma omp parallel private(tid)
+  #pragma omp parallel private(tid) shared(prefix_sum)
   {
   tid = omp_get_thread_num();
   p =  omp_get_num_threads();
   long chunksize, start, stop;
-  chunksize = ceil(n/p); // will have to deal with potential for last chunk to be smaller
+  chunksize = ceil((double)n/(double)p); // will have to deal with potential for last chunk to be smaller
+  start = tid*chunksize;
+  stop = std::min(start + chunksize, n); //in case n not divisible by p. used ceil, so min
+  //printf("n %ld, p %d\n", n, p);
+  //printf("chunksize=%ld, start=%ld, stop=%ld\n", chunksize, start, stop);
   if (tid==0){
       printf("Num threads: %d\n", p);
       prefix_sum[0] = 0;
@@ -33,18 +41,17 @@ void scan_omp(long* prefix_sum, const long* A, long n) {
   }
   printf("Set up chunks\n");
   // each will have its own prefix sum
-  start = p*chunksize;
-  stop = std::min(start + chunksize, n); //in case n not divisible by p. used ceil, so min
   
   //prefix_sum[start] = A[start]; // should be this if serial is how i think it should be!
   for (long i=start+1; i<stop; i++){
       prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+      //printf("p[%ld] = %ld\n", i, prefix_sum[i]);
   }
   printf("Thread %d computed its chunk\n", tid);
   #pragma omp barrier
   //} //end pragma region
   // all threads have finished their chunk
-  
+   
   //now correct for the constant 
   // doing this because we still need chunksize and p
   if (tid==0){
@@ -53,13 +60,16 @@ void scan_omp(long* prefix_sum, const long* A, long n) {
       long const_index;
       for (long i=0; i<n; i++){
           // sum over the contants from chunks below yours
-          // what chunk was this part in?
-          t = floor(i/chunksize); //should be equiv to tid
-          for(int l=0; l<t; l++){
-              // the constant to add is the value before "stop"
-              const_index = std::min((p+1)*chunksize, n)-1;
-              prefix_sum[i] += prefix_sum[const_index];
+          // figure out what chunk t this part was in
+          t = floor(i/chunksize); //should be equiv to orig tid
+          // the constant to add is the value before "stop";
+          // only need to add one bc we will have already
+          // fixed all the earlier values
+          if (t>0){
+            const_index = std::min(t*chunksize, n)-1;
+            prefix_sum[i] += prefix_sum[const_index];
           }
+          //printf("pfinal[%ld] = %ld\n", i, prefix_sum[i]);
       }
   }
   } //end pragma region
