08d2505ef3a6a54ae13001c564fb5e7ab71516ef
Kate Storey-Fisher <k.sf@nyu.edu>
2021-03-28 16:26:13 -0400

    solved bug4

--- hw3/omp_solved4.c
+++ hw3/omp_solved4.c
@@ -9,27 +9,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 1048
-//#define N 4
 
 int main (int argc, char *argv[]) 
 {
 int nthreads, tid, i, j;
-double a[N][N];
-void compute_a(int i, int j, double a[N][N], int tid);
+// Change a to a pointer to an int array.
+//double a[N][N];
+int *a;
 
-printf("start\n");
-//#pragma stack, 10000000
-#pragma comment(linker, "/STACK:20000000")
 /* Fork a team of threads with explicit variable scoping */
 #pragma omp parallel shared(nthreads) private(i,j,tid,a)
   {
-  printf("here\n");
+  // BUG/FIX: There is not enough stack space for each 
+  // thread to have its own copy of a on the stack. We 
+  // thus need to malloc a. We also convert a to an int
+  // so that it takes up less memory, as it will only be 
+  // filled with ints anyway (sum of i, j, tid, all ints).
+  a = (int*) malloc(N*N*sizeof(int));
+
   /* Obtain/print thread info */
   tid = omp_get_thread_num();
-  printf("posttid\n");
   if (tid == 0) 
     {
-    printf("tid0\n");
     nthreads = omp_get_num_threads();
     printf("Number of threads = %d\n", nthreads);
     }
@@ -38,18 +39,19 @@ printf("start\n");
   /* Each thread works on its own private copy of the array */
   for (i=0; i<N; i++)
     for (j=0; j<N; j++)
-      a[i][j] = tid + i + j;
-  
+      //a[i][j] = tid + i + j;
+      // We need to properly access the values of a for
+      // a malloced array
+      a[i*N+j] = tid + i + j;
+
   /* For confirmation */
-  printf("Thread %d done. Last element= %f\n",tid,a[N-1][N-1]);
+  // Fixed this printline to print out an integer, and access
+  // the last value of a appropriately
+  printf("Thread %d done. Last element= %d\n",tid,a[N*N-1]);
 
   }  /* All threads join master thread and disband */
 
+  // We now have to free a
+  free(a);
 }
 
-/*void compute_a(int i, int j, double a[N][N], int tid)
-{
-    for (i=0; i<N; i++)
-      for (j=0; j<N; j++)
-        a[i][j] = tid + i + j;
-}*/
