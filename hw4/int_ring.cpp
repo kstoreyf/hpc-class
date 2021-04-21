bd8c67ed2532b2f8c21c152a8c45d81d887dc3d5
Kate Storey-Fisher <k.sf@nyu.edu>
2021-04-21 15:07:49 -0400

    included all parts of int ring problem

--- hw4/int_ring.cpp
+++ hw4/int_ring.cpp
@@ -6,7 +6,7 @@
 #include <mpi.h>
 #include <iostream>
 
-double ring(long N_loops, int* msg, MPI_Comm comm) {
+double ring(long N_loops, int* msg, int msg_size, MPI_Comm comm) {
   int rank, p;
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
@@ -20,34 +20,24 @@ double ring(long N_loops, int* msg, MPI_Comm comm) {
   for (long repeat  = 0; repeat < N_loops; repeat++) {
     MPI_Status status;
     // Only rank 0 will print this
-    if (!rank) printf("Loop %d/%d\n", repeat, N_loops);
+    //if (!rank) printf("Loop %d/%d\n", repeat, N_loops);
     for (int r=0; r<p; r++){
       proc_current = r;
       proc_next = (proc_current + 1) % p;
       if (rank == proc_current) {
-        *msg += rank;
-        printf("Loop %d: Proc %d just added to msg, now msg=%d. Sending to proc %d\n", repeat, rank, *msg, proc_next);
-        MPI_Send(msg, 1, MPI_INT, proc_next, repeat, comm);
+        // This is the single integer case, so add to it.
+        // Otherwise for the array case, don't need to modify.
+        if (msg_size==1) {
+          *msg += rank;
+        }
+        //printf("Loop %d: Proc %d just added to msg, now msg=%d. Sending to proc %d\n", repeat, rank, *msg, proc_next);
+        MPI_Send(msg, msg_size, MPI_INT, proc_next, repeat, comm);
       }
       else if (rank == proc_next) {
-        MPI_Recv(msg, 1, MPI_INT, proc_current, repeat, comm, &status);
-        printf("Loop %d: Proc %d just received msg (msg=%d)\n", repeat, rank, *msg);
+        MPI_Recv(msg, msg_size, MPI_INT, proc_current, repeat, comm, &status);
+        //printf("Loop %d: Proc %d just received msg (msg=%d)\n", repeat, rank, *msg);
       }
     }
-    //if (rank == proc_current) {
-    //  // add own rank to message
-    //  msg += rank;
-    //  printf("Proc %d just added to msg, now msg=%d. Sending to proc %d\n", rank, msg, proc_next);
-    //  // 1 is size; single integer so just 1
-    //  // repeat is the message tag, should be unique
-    //  MPI_Send(&msg, 1, MPI_INT, proc_next, repeat, comm);
-    //}
-    //else if (rank == proc_next) {
-    //  MPI_Recv(&msg, 1, MPI_INT, proc_current, repeat, comm, &status);
-    //  printf("Proc %d just received msg (msg=%d)\n", rank, msg);
-    //  proc_current = proc_next;
-    //  proc_next = proc_current + 1;
-    //}
 
   }
   tt = MPI_Wtime() - tt;
@@ -55,14 +45,23 @@ double ring(long N_loops, int* msg, MPI_Comm comm) {
   return tt;
 }
 
+
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
 
-  if (argc < 2) {
-    printf("Usage: mpirun ./int_ring <N_loops>\n");
+  if (argc < 3) {
+    printf("Usage: mpirun ./int_ring <N_loops> <single_int>\n");
     abort();
   }
   long N_loops = atol(argv[1]);
+  // Single_int should be 0 or 1; if 0, add to int
+  // if 1, pass array
+  int single_int = atoi(argv[2]);
+  if (single_int!=0){
+    if (single_int!=1){
+      printf("single_int must be 0 or 1!\n");
+    }
+  }
 
   int rank, p;
   MPI_Comm comm = MPI_COMM_WORLD;
@@ -75,25 +74,38 @@ int main(int argc, char** argv) {
   MPI_Get_processor_name(processor_name, &name_len);
   printf("Rank %d/%d running on %s.\n", rank, p, processor_name);
 
-  int msg = 0;
-  double tt = ring(N_loops, &msg, comm);
-  MPI_Finalize();
-  if (!rank) printf("final message: %d\n", msg);
+  double tt;
+  // Pass single integer
+  if (single_int) {
+    printf("Passing integer around network\n");
+    int msg = 0;
+    int msg_size = 1;
+    tt = ring(N_loops, &msg, msg_size, comm);
+    MPI_Finalize();
 
-  /* Compute expected message */
-  int msg_true = 0;
-  for (int i=0; i<p; i++){
-    msg_true += i;
+    if (!rank) printf("final message: %d\n", msg);
+    /* Compute expected message */
+    int msg_true = 0;
+    for (int i=0; i<p; i++){
+      msg_true += i;
+    }
+    msg_true *= N_loops;
+    if (!rank) printf("expected message: %d\n", msg_true);
+    if (!rank) printf("ring latency: %e ms\n", tt/(N_loops*p) * 1000);
   }
-  msg_true *= N_loops;
-  if (!rank) printf("expected message: %d\n", msg_true);
-  
-  if (!rank) printf("ring latency: %e ms\n", tt/N_loops * 1000);
-
-  //Nrepeat = 10000;
-  //long Nsize = 1000000;
-  //tt = time_pingpong(proc0, proc1, Nrepeat, Nsize, comm);
-  //if (!rank) printf("pingpong bandwidth: %e GB/s\n", (Nsize*Nrepeat)/tt/1e9);
+  // OR: Pass array
+  else {
+    // int = 4 bytes; 5e5 ints = 2MB
+    long msg_size = 500000;
+    printf("Passing array of %d ints (%f MB) around network loop\n", msg_size, msg_size*sizeof(int)/1e6);
+    int* msg_arr = (int*) malloc(msg_size*sizeof(int));
+    for (long i = 0; i < msg_size; i++) msg_arr[i] = 42;
+    tt = ring(N_loops, msg_arr, msg_size, comm);
+    MPI_Finalize();
+    free(msg_arr);
+    if (!rank) printf("ring latency: %e ms\n", tt/(N_loops*p) * 1000);
+    if (!rank) printf("ring bandwidth: %e GB/s\n", (sizeof(int)*msg_size*N_loops*p)/tt/1e9);
+  }
 
 }
 
